32262d0aed5b2421cb169f3f62ad57503968e12e
mushfiq <mushfiqxrabbi@gmail.com>
2023-03-30 20:35:46 +0600

    add dynamic cors

--- index.js
+++ index.js
@@ -4,25 +4,33 @@ const fs = require("fs");
 
 const ipcData = JSON.parse(fs.readFileSync("./ipcData.json", "utf8"));
 
+const corsWhitelist = [
+  "http://localhost:3000",
+  "https://mushfiqrabbi.github.io",
+];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (corsWhitelist.includes(origin)) {
+      callback(null, origin);
+    } else {
+    }
+  },
+};
+
 const ipcRouter = express.Router();
 
-ipcRouter.get(
-  "/:category",
-  cors({
-    origin: "https://mushfiqrabbi.github.io",
-  }),
-  (req, res) => {
-    const requestedData = ipcData[Number(req.params.category) - 1];
-    if (req.query.yearly === "true") {
-      res.json({
-        ...requestedData,
-        amount: requestedData.amount - requestedData.amount * 0.25,
-      });
-    } else {
-      res.json(requestedData);
-    }
+ipcRouter.get("/:category", cors(corsOptions), (req, res) => {
+  const requestedData = ipcData[Number(req.params.category) - 1];
+  if (req.query.yearly === "true") {
+    res.json({
+      ...requestedData,
+      amount: requestedData.amount - requestedData.amount * 0.25,
+    });
+  } else {
+    res.json(requestedData);
   }
-);
+});
 
 const app = express();
 const PORT = process.env.PORT || 5000;
